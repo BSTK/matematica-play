1364c4d591b4421cf686e89fb6356e2f0814f90a
Bruno Luz <bsilva.se@gmail.com>
2021-04-27 20:13:39 -0300

    main - Primeiro microserviço

---
+++ operacao-matematica/src/main/java/com/caqqi/matematicaplay/operacao/OperacaoMatematicaApplication.java
@@ -0,0 +1,13 @@
+package com.caqqi.matematicaplay.operacao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OperacaoMatematicaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OperacaoMatematicaApplication.class, args);
+	}
+
+}
---
+++ operacao-matematica/src/test/java/com/caqqi/matematicaplay/operacao/OperacaoMatematicaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.caqqi.matematicaplay.operacao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OperacaoMatematicaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
