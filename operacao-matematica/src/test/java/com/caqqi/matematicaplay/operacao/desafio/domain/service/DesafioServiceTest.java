9fa523993980699e23ccfcc37918dd00a3024121
Bruno Luz <bsilva.se@gmail.com>
2021-06-11 20:50:14 -0300

    main - Adicionando casos de testes

--- operacao-matematica/src/test/java/com/caqqi/matematicaplay/operacao/desafio/domain/service/DesafioServiceTest.java
+++ operacao-matematica/src/test/java/com/caqqi/matematicaplay/operacao/desafio/domain/service/DesafioServiceTest.java
@@ -3,6 +3,7 @@ package com.caqqi.matematicaplay.operacao.desafio.domain.service;
 import com.caqqi.matematicaplay.operacao.desafio.api.request.DesafioTentativaRespostaRequest;
 import com.caqqi.matematicaplay.operacao.desafio.domain.entidade.DesafioTentativaResposta;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.BDDAssertions.then;
@@ -17,22 +18,84 @@ public class DesafioServiceTest {
     }
 
     @Test
-    public void verificaTentativaDeRespostaCorreta() {
+    @DisplayName("Verifica tentativa de resposta correta operacao soma")
+    public void verificaTentativaDeRespostaCorretaOperacaoSoma() {
         DesafioTentativaRespostaRequest tentativaRequest = new DesafioTentativaRespostaRequest(
-            10, 10, 20, "+", "usuarioAAB");
+            10, 10, 20, "+", "UsuarioAAB");
 
-        DesafioTentativaResposta desafioTentativaResposta = desafioService.verificarResposta(tentativaRequest);
-
-        then(desafioTentativaResposta.isCorreta()).isTrue();
+        executaValidacaoCorreta(tentativaRequest);
     }
 
     @Test
-    public void verificaTentativaDeRespostaErrada() {
+    @DisplayName("Verifica tentativa de resposta errada operacao soma")
+    public void verificaTentativaDeRespostaErradaOperacaoSoma() {
         DesafioTentativaRespostaRequest tentativaRequest = new DesafioTentativaRespostaRequest(
-            10, 10, 25, "+", "usuarioAAB");
+            10, 10, 25, "+", "UsuarioAAB");
 
-        DesafioTentativaResposta desafioTentativaResposta = desafioService.verificarResposta(tentativaRequest);
+        executaValidacaoErrada(tentativaRequest);
+    }
 
+    @Test
+    @DisplayName("Verifica tentativa de resposta correta operacao subtração")
+    public void verificaTentativaDeRespostaCorretaOperacaoSubtracao() {
+        DesafioTentativaRespostaRequest tentativaRequest = new DesafioTentativaRespostaRequest(
+            10, 10, 0, "-", "UsuarioAAB");
+
+        executaValidacaoCorreta(tentativaRequest);
+    }
+
+    @Test
+    @DisplayName("Verifica tentativa de resposta errada operacao subtração")
+    public void verificaTentativaDeRespostaErradaOperacaoSubtracao() {
+        DesafioTentativaRespostaRequest tentativaRequest = new DesafioTentativaRespostaRequest(
+            10, 10, 25, "-", "UsuarioAAB");
+
+        executaValidacaoErrada(tentativaRequest);
+    }
+
+    @Test
+    @DisplayName("Verifica tentativa de resposta correta operacao divisao")
+    public void verificaTentativaDeRespostaCorretaOperacaoDivisao() {
+        DesafioTentativaRespostaRequest tentativaRequest = new DesafioTentativaRespostaRequest(
+            10, 10, 1, "/", "UsuarioAAB");
+
+        executaValidacaoCorreta(tentativaRequest);
+    }
+
+    @Test
+    @DisplayName("Verifica tentativa de resposta errada operacao divisao")
+    public void verificaTentativaDeRespostaErradaOperacaoDivisao() {
+        DesafioTentativaRespostaRequest tentativaRequest = new DesafioTentativaRespostaRequest(
+            10, 10, 0, "/", "UsuarioAAB");
+
+        executaValidacaoErrada(tentativaRequest);
+    }
+
+    @Test
+    @DisplayName("Verifica tentativa de resposta correta operacao multiplicacao")
+    public void verificaTentativaDeRespostaCorretaOperacaoMultiplicacao() {
+        DesafioTentativaRespostaRequest tentativaRequest = new DesafioTentativaRespostaRequest(
+            10, 10, 100, "*", "UsuarioAAB");
+
+        executaValidacaoCorreta(tentativaRequest);
+    }
+
+    @Test
+    @DisplayName("Verifica tentativa de resposta errada operacao multiplicacao")
+    public void verificaTentativaDeRespostaErradaOperacaoMultiplicacao() {
+        DesafioTentativaRespostaRequest tentativaRequest = new DesafioTentativaRespostaRequest(
+            10, 10, 99, "*", "UsuarioAAB");
+
+        executaValidacaoErrada(tentativaRequest);
+    }
+
+    private void executaValidacaoCorreta(final DesafioTentativaRespostaRequest request) {
+        DesafioTentativaResposta desafioTentativaResposta = desafioService.verificarResposta(request);
+        then(desafioTentativaResposta.isCorreta()).isTrue();
+    }
+
+    private void executaValidacaoErrada(final DesafioTentativaRespostaRequest request) {
+        DesafioTentativaResposta desafioTentativaResposta = desafioService.verificarResposta(request);
         then(desafioTentativaResposta.isCorreta()).isFalse();
     }
 }
