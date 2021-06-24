cbdb9d81146c2d2fd4e0aaae7c03f0a47a25f8fe
Bruno Luz <bsilva.se@gmail.com>
2021-06-23 22:29:21 -0300

    main - Adicionando configuração de Cors

--- operacao-matematica/src/main/java/com/caqqi/matematicaplay/operacao/core/WebConfig.java
+++ operacao-matematica/src/main/java/com/caqqi/matematicaplay/operacao/core/WebConfig.java
@@ -1,9 +1,17 @@
 package com.caqqi.matematicaplay.operacao.core;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+@EnableWebMvc
 @Configuration
-public class Configuracao {
+public class WebConfig implements WebMvcConfigurer {
 
-    
+    @Override
+    public void addCorsMappings(final CorsRegistry registry) {
+        registry.addMapping("/**");
+    }
+
 }
