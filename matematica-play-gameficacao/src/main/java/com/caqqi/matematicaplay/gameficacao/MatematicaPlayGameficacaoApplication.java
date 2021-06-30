01ace05ce2b5f015ac84e1a3c73b9adf0e7f4659
Bruno Luz <bsilva.se@gmail.com>
2021-06-30 20:40:24 -0300

    main - Criando o microserviço de Gamificação

---
+++ matematica-play-gameficacao/src/main/java/com/caqqi/matematicaplay/gameficacao/MatematicaPlayGameficacaoApplication.java
@@ -0,0 +1,13 @@
+package com.caqqi.matematicaplay.gameficacao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MatematicaPlayGameficacaoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MatematicaPlayGameficacaoApplication.class, args);
+    }
+
+}
---
+++ matematica-play-gameficacao/src/test/java/com/caqqi/matematicaplay/gameficacao/MatematicaPlayGameficacaoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.caqqi.matematicaplay.gameficacao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MatematicaPlayGameficacaoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
