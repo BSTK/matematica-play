2ca1bb57fd37f5a8e0a121e16eed1ae6e2aa782c
Bruno Luz <bsilva.se@gmail.com>
2021-06-30 21:16:48 -0300

    main - Criando a camada de dominio

---
+++ matematica-play-gameficacao/src/main/java/com/caqqi/matematicaplay/domain/entity/ScoreCard.java
@@ -0,0 +1,42 @@
+package com.caqqi.matematicaplay.domain.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Data
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "SCORE_CARD")
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+public class ScoreCard {
+
+    private static final int PONTOS_DEFAULT = 10;
+
+    @Id
+    @GeneratedValue
+    @EqualsAndHashCode.Include
+    private Long id;
+
+    @Column(name = "USUARIO_ID")
+    private Long usuarioId;
+
+    @Column(name = "DESAFIO_TENTAIVA_ID")
+    private Long desafioTentaivaId;
+
+    @Column(name = "DATA_INSERT")
+    private LocalDateTime dataInsert;
+
+    @Column(name = "PONTOS")
+    private int pontos;
+
+    public ScoreCard(final Long usuarioId,
+                     final Long desafioTentaivaId) {
+        this(null, usuarioId, desafioTentaivaId, LocalDateTime.now(), PONTOS_DEFAULT);
+    }
+}
---
+++ matematica-play-gameficacao/src/main/java/com/caqqi/matematicaplay/domain/enums/BadgeTipo.java
@@ -0,0 +1,17 @@
+package com.caqqi.matematicaplay.domain.enums;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public enum BadgeTipo {
+
+    BRONZE("Bronze"),
+    PRATA("Prata"),
+    OURO("Ouro"),
+    PRIMEIRO_ACERTO("Primeira tentica correta"),
+    NUMERO_DA_SORTE("Número da sorte");
+
+    private final String descricao;
+}
