7825ed32772ffde1b677d289aa6538f9f05bda06
Bruno Luz <bsilva.se@gmail.com>
2021-07-16 13:27:29 -0300

    main - Commit Quality Gate

--- matematica-play-frontend/src/app/core/core.module.ts
+++ matematica-play-frontend/src/app/core/core.module.ts
@@ -1,9 +1,9 @@
 import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
-import {FooterComponent} from './footer/footer.component';
-import {HeaderComponent} from './header/header.component';
 import {RouterModule} from '@angular/router';
 import {HttpClientModule} from '@angular/common/http';
+import {FooterComponent} from './footer/footer.component';
+import {HeaderComponent} from './header/header.component';
 
 @NgModule({
   declarations: [
